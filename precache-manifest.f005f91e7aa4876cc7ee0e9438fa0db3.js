52396e1cdc66fb285ce4276696f5ef4c617b706c
Edward Phillips <edphil@hotmail.co.uk>
2020-01-25 22:45:29 +0100

    Updates

---
+++ precache-manifest.f005f91e7aa4876cc7ee0e9438fa0db3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d812c65272ef55c9b3a78895a89c5d1",
+    "url": "/react-drums/index.html"
+  },
+  {
+    "revision": "72e3b099ced672781d3f",
+    "url": "/react-drums/static/css/main.44f28845.chunk.css"
+  },
+  {
+    "revision": "349e99a77dee91bc1860",
+    "url": "/react-drums/static/js/2.a9eaa1cb.chunk.js"
+  },
+  {
+    "revision": "a4238d9b9f29bfeea1cc4e1b5f7ba331",
+    "url": "/react-drums/static/js/2.a9eaa1cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72e3b099ced672781d3f",
+    "url": "/react-drums/static/js/main.8992e0d8.chunk.js"
+  },
+  {
+    "revision": "8e67e2105b9cdc9ff82d",
+    "url": "/react-drums/static/js/runtime-main.47fa5ed6.js"
+  },
+  {
+    "revision": "ccba712cb849b95ada314b72f703288b",
+    "url": "/react-drums/static/media/HiHatClosed_VintageIndie.ccba712c.wav"
+  },
+  {
+    "revision": "71b2cf7392084a0868b59de93dd9c1f6",
+    "url": "/react-drums/static/media/HiHatOpen_VintageIndie.71b2cf73.wav"
+  },
+  {
+    "revision": "fcdae127b603f5dde2cfd542ee1d762e",
+    "url": "/react-drums/static/media/Kick_VintageIndie.fcdae127.wav"
+  },
+  {
+    "revision": "09d28e60b1ad801ddc89f3fe3e503dbf",
+    "url": "/react-drums/static/media/RideBell_VintageIndie.09d28e60.wav"
+  },
+  {
+    "revision": "1b44ebe7ef9230b0b8f52fcc824795f9",
+    "url": "/react-drums/static/media/Ride_VintageIndie.1b44ebe7.wav"
+  },
+  {
+    "revision": "5bbd4d6fce6cdb4d49dcf83c8b2301bc",
+    "url": "/react-drums/static/media/Snare_VintageIndie.5bbd4d6f.wav"
+  },
+  {
+    "revision": "46b4c8d78378a4587815da05c73d27a7",
+    "url": "/react-drums/static/media/Tom_VintageIndie.46b4c8d7.wav"
+  },
+  {
+    "revision": "c09137cf242cfbe045757f4982e6e169",
+    "url": "/react-drums/static/media/XStick_VintageIndie.c09137cf.wav"
+  }
+]);
